53ee722f6c5754c6f96df012b4d86ed81630649e
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2021-12-18 00:52:35 +0100

    a

--- components/MoreResults.jsx
+++ components/MoreResults.jsx
@@ -1,12 +1,17 @@
 import Icon from "./common/Icon";
+import {useRouter} from 'next/router';
 
-const MoreResults = ({ destination, price, date, buddies }) => {
+const MoreResults = ({images, slug, destination, price, date, buddies }) => {
+  const {push} = useRouter();
+  function goToTrip(slug) {
+    push(`/trip/${slug}`)
+  }
   return (
-    <div className="trip-card p-4 my-6 flex flex-col md:flex-row border rounded border-gray-light3">
+    <div onClick={goToTrip} className="trip-card p-4 my-6 flex flex-col md:flex-row border rounded border-gray-light3">
       <div className="md:mr-5 md:w-1/4 flex-none">
         <div
           className="pt-2/3 bg-no-repeat bg-cover rounded"
-          style={{ backgroundImage: "url('/landscape.jpg')" }}
+          style={{ backgroundImage:  `url(${images && images[0] ? images[0]: "./landscape.png"})` }}
         ></div>
       </div>
       <div className="trip-card__details pt-4">
