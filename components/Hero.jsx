4a66edbec306e78ef15eea77662ce6ba9ec36735
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2021-12-19 12:33:27 +0100

    fix chat issue

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -27,7 +27,28 @@ const Hero = () => {
     }
     return (
 
-      <div className="">
+      <div className="relative">
+        <svg className="absolute" width="1283" height="712" viewBox="0 0 1283 712" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="1269.44" cy="14.1689" r="13.0144" fill="#F3D7D5"/>
+          <circle cx="1269.44" cy="14.1689" r="4.82016" fill="#EA4524"/>
+          <path d="M1270.44 14.1689C778.391 -29.3795 318.38 3.6572 1.52832 710.942" stroke="url(#paint0_linear_236_837)" stroke-opacity="0.2" stroke-width="1.40156" stroke-dasharray="4 4"/>
+          <g clip-path="url(#clip0_236_837)">
+          <path d="M691.496 40.0586C691.339 39.4926 690.706 39.1346 690.14 39.2915L686.249 40.3699L681.143 35.6902L679.728 36.0824L683.065 41.2523L679.174 42.3307L677.72 41.2098L676.659 41.5039L678.053 43.7841L678.032 46.4565L679.093 46.1624L679.762 44.4532L683.653 43.3748L683.453 49.5251L684.868 49.1329L686.837 42.4924L690.728 41.414C691.294 41.2571 691.652 40.6246 691.496 40.0586Z" fill="url(#paint1_linear_236_837)"/>
+          </g>
+          <defs>
+          <linearGradient id="paint0_linear_236_837" x1="1246.91" y1="12.1667" x2="1.02785" y2="622.844" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#EA4524"/>
+          <stop offset="1" stop-color="#EA4524" stop-opacity="0"/>
+          </linearGradient>
+          <linearGradient id="paint1_linear_236_837" x1="705.583" y1="35.1578" x2="675.41" y2="43.1859" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#F2856F"/>
+          <stop offset="1" stop-color="#FFEBE7"/>
+          </linearGradient>
+          <clipPath id="clip0_236_837">
+          <rect width="14.6836" height="14.6836" fill="white" transform="translate(675.385 36.9055) rotate(-15.4903)"/>
+          </clipPath>
+          </defs>
+        </svg>
         <div className="bg-hero bg-no-repeat bg-cover lg:h-100v md:h-60v h-60">
           <div className="relative container md:p-16 p-5 flex justify-between items-center">
             
--- pages/messaging/[slug]/index.jsx
+++ pages/messaging/[slug]/index.jsx
@@ -148,7 +148,7 @@ export default Messaging;
 export async function getServerSideProps(context) {
   const { slug } = context.query;
 
-  const tripData = await getRequest(`${proces.env.NEXT_PUBLIC_API_LOCATION}/trip/by/slug/${slug}`);
+  const tripData = await getRequest(`${process.env.NEXT_PUBLIC_API_LOCATION}/trip/by/slug/${slug}`);
   
   if (tripData.status) {
     const tripMessages = await getRequest(`/trip/${tripData.data.id}/chats`);
