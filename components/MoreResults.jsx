ec969fcda0470f34989210c3e2c59d129df06ffe
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2021-12-27 14:57:22 +0100

    a

--- components/MoreResults.jsx
+++ components/MoreResults.jsx
@@ -18,21 +18,20 @@ const MoreResults = (prop) => {
       <div className="trip-card__details w-8/12">
         <h2 className="text-black-content font-circular-black text-base md:text-2xl flex justify-between">
           <span className="">{prop.trip.destination}</span> 
-          <span>${prop.trip.travel_amount + prop.trip.miscellaneous_amount + prop.trip.accommodation_amount }</span>
+          <span className="">${prop.trip.travel_amount + prop.trip.miscellaneous_amount + prop.trip.accommodation_amount }</span>
 
         </h2>
         <div className="flex justify-between">
-          <span className="text-sm">{moment(prop.trip.start_date).format("dddd, MMMM DD, YYYY")}</span>
-          <span className="text-sm">{prop.trip.buddies} buddies</span>
+          <span className=" text-gray">{moment(prop.trip.start_date).format("dddd, MMMM DD, YYYY")}</span>
+          <span className=" text-gray">{prop.trip.buddies} buddies</span>
         </div>
         <div>
         <div className="buddies-checklist mt-10">
-          <h2 className="font-circular-bold">Buddies Checklist</h2>
           <div className="buddies-list grid md:grid-cols-3 md:gap-8 grid-cols-2">
             {prop.trip.checklists && prop.trip.checklists.map((item, index) => (
               <div className="flex items-center" key={index}>
                 <Icon icon="checkmark" cname="flex-none" />
-                <p className="pl-1 whitespace-nowrap">{item}</p>
+                <p className="pl-1 whitespace-nowrap text-gray">{item}</p>
               </div>
             ))}
           </div>
