5bcf4f2d71373dee07ba1efb7c61613668481de6
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2021-12-28 22:56:57 +0100

    more destination

--- components/MoreDestinationCard.jsx
+++ components/MoreDestinationCard.jsx
@@ -12,51 +12,51 @@ const MoreDestinationCard = ({ props }) => {
     <>
       <div className="card-wrapper grid grid-cols-8 gap-5 container">
         {/* Card 1 */}
-        <div onClick={() => goToDestination(props[4] && props[4].name)} className="card rounded-10 overflow-hidden col-span-2 h-500 w-full max-w-sm bg-no-repeat bg-center bg-cover cursor-pointer" style={{backgroundImage: `url(${props[4] && props[4].image})`}}>
+        <div onClick={() => goToDestination(props[0] && props[0].name)} className="card rounded-10 overflow-hidden col-span-2 h-500 w-full max-w-sm bg-no-repeat bg-center bg-cover cursor-pointer" style={{backgroundImage: `url(${props[0] && props[0].image})`}}>
           <div className="card-text relative top-48 lg:top-80">
             <p className="font-circular-black lg:text-40 text-26 text-white">
-              {props[4] && props[4].name}
+              {props[0] && props[0].name}
             </p>
             <p className="md:text-14 text-xs text-white uppercase">
-              {props[4] && props[4].location}
+              {props[0] && props[0].location}
             </p>
           </div>
         </div>
 
         {/* Card 2,3,4 wrapper */}
-        <div onClick={() => goToDestination(props[5] && props[5].name)} className="grid grid-cols-2 gap-5 col-span-6 cursor-pointer">
+        <div onClick={() => goToDestination(props[1] && props[1].name)} className="grid grid-cols-2 gap-5 col-span-6 cursor-pointer">
           {/* Card 2 */}
-          <div className="card rounded-lg relative overflow-hidden h-60 w-full max-w-2xl bg-no-repeat bg-center bg-cover"  style={{backgroundImage: `url(${props[5] && props[5].image})`}}>
+          <div className="card rounded-lg relative overflow-hidden h-60 w-full max-w-2xl bg-no-repeat bg-center bg-cover"  style={{backgroundImage: `url(${props[1] && props[1].image})`}}>
             <div className="card-text absolute top-24 left-24">
               <p className="font-circular-black lg:text-40 text-26 text-white">
-                {props[5] && props[5].name}
+                {props[1] && props[1].name}
               </p>
               <p className="md:text-14 text-xs text-white uppercase">
-                {props[5] && props[5].location}
+                {props[1] && props[1].location}
               </p>
             </div>
           </div>
 
           {/* Card 3 */}
-          <div onClick={() => goToDestination(props[6] && props[6].name)} className="card rounded-10 relative overflow-hidden h-60 w-full max-w-2xl bg-no-repeat bg-center bg-cover cursor-pointer "  style={{backgroundImage: `url(${props[6] && props[6].image})`}}>
+          <div onClick={() => goToDestination(props[2] && props[2].name)} className="card rounded-10 relative overflow-hidden h-60 w-full max-w-2xl bg-no-repeat bg-center bg-cover cursor-pointer "  style={{backgroundImage: `url(${props[2] && props[2].image})`}}>
             <div className="card-text absolute top-24 left-24">
               <p className="font-circular-black lg:text-40 text-26 text-white">
-                {props[6] && props[6].name}
+                {props[2] && props[2].name}
               </p>
               <p className="md:text-14 text-xs text-white uppercase">
-                {props[6] && props[6].location}
+                {props[2] && props[2].location}
               </p>
             </div>
           </div>
 
           {/* Card 4 */}
-          <div  onClick={() => goToDestination(props[7] && props[7].name)} className="col-span-2 card rounded-10 relative overflow-hidden h-72 w-full max-w-6xl bg-no-repeat bg-cover cursor-pointer"  style={{backgroundImage: `url(${props[7] && props[7].image})`}}>
+          <div  onClick={() => goToDestination(props[3] && props[3].name)} className="col-span-2 card rounded-10 relative overflow-hidden h-72 w-full max-w-6xl bg-no-repeat bg-cover cursor-pointer"  style={{backgroundImage: `url(${props[3] && props[3].image})`}}>
             <div className="card-text absolute top-24 left-72">
               <p className="font-circular-black lg:text-40 text-26 text-white">
-                {props[7] && props[7].name}
+                {props[3] && props[3].name}
               </p>
               <p className="md:text-14 text-xs text-white uppercase">
-                {props[7] && props[7].location}
+                {props[3] && props[3].location}
               </p>
             </div>
           </div>
