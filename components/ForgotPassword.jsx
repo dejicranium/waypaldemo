2d3ce37f99795721e742faa4c9a6672bf2e4ef4b
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2022-01-08 00:32:25 +0100

    fix forgot password

--- components/ForgotPassword.jsx
+++ components/ForgotPassword.jsx
@@ -17,17 +17,31 @@ const ForgotPassword = ({ setActive }) => {
   const [error, setError] = useState(null);
   const [message, setMessage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [show_token, setShowToken] = useState(false);
 
   const submit = async (values) => {
     setLoading(true);
-    const user = await postRequest("/user/sendForgotten", values);
-    if (user.status) {
-      Mixpanel.track("password-forgotten-successful");
-      setMessage(user.message);
-      reset();
-    } else {
-      Mixpanel.track("password-forgotten-failed");
-      setError(user.message);
+    if (!show_token) {
+      const user = await postRequest("/user/sendForgotten", values);
+      if (user.status) {
+        Mixpanel.track("password-forgotten-successful");
+        setMessage(user.message);
+        setShowToken(true)
+        reset();
+      } else {
+        Mixpanel.track("password-forgotten-failed");
+        setError(user.message);
+      }
+    }
+    else {
+      const update = await postRequest('/user/resetPassword', values);
+      if (update.status) {
+        setMessage("Password reset successfully");
+        setActive("login");
+      }
+      else {
+        setError(update.message);
+      }
     }
     setLoading(false);
   };
@@ -75,13 +89,56 @@ const ForgotPassword = ({ setActive }) => {
             helptext={errors.email && errors.email.message}
             helptextstyle={errors.email && "text-red-500"}
           />
-          <Button
-            type="submit"
-            btnType="fill"
-            btnText="Send me an email"
-            btnStyle="w-full mb-3"
-            loading={loading}
-          />
+          {show_token && (
+            <InputField
+              id="reset-token"
+              type="text"
+              placeholder="Token"
+              className="mb-6"
+              innerref={register("token", {
+                required: {
+                  value: true,
+                  message: "Please enter valid token",
+                },
+              })}
+              helptext={errors.token && errors.token.message}
+              helptextstyle={errors.token && "text-red-500"}
+            />
+          )}
+          {show_token && (
+            <InputField
+              id="reset-password"
+              type="password"
+              placeholder="New password"
+              className="mb-6"
+              innerref={register("password", {
+                required: {
+                  value: true,
+                  message: "Please enter valid password",
+                },
+              })}
+              helptext={errors.password && errors.password.message}
+              helptextstyle={errors.password && "text-red-500"}
+            />
+          )}
+          {!show_token && (
+            <Button
+              type="submit"
+              btnType="fill"
+              btnText="Send me an email"
+              btnStyle="w-full mb-3"
+              loading={loading}
+            />
+          )}
+          {show_token && (
+              <Button
+              type="submit"
+              btnType="fill"
+              btnText="Reset password"
+              btnStyle="w-full mb-3"
+              loading={loading}
+            />
+          )}
           <Button
             type="button"
             btnType="outline"
