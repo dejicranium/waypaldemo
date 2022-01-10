c7c934c812048de2c95f2f04382e6603950e673c
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2022-01-11 00:01:24 +0100

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { log } = require("console");
 const next = require("next");
 const app = require("express")();
 const server = require("http").Server(app);
-
+const expsession = require('express-session');
 const io = require("socket.io")(server, {
   cors: {
     origin: '*'
@@ -13,9 +13,24 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
+const sessionMiddleware = expsession({
+  secret: 'ra23432#@#fewrwo',
+  saveUninitialized: true,
+  resave: true
+});
+
+app.use(sessionMiddleware);
+
+io.use(function(socket, next) {
+  sessionMiddleware(socket.request, socket.request.res, next);
+})
+
+
 let port = 3000;
 
 io.on("connection", (socket) => {
+  socket.request.session.socketio = socket.id;
+  socket.request.session.save();  
   socket.on("join_room", async (room) => {
     socket.join(room);
   });
@@ -25,6 +40,13 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+app.use(function(req, res, next) {
+  const session = req.session;
+  if (!session.cntr) session.cntr = 0;
+  ++session.cntr;
+  next();
+})
 
 nextApp.prepare().then(() => {
   app.get("*", (req, res) => {
